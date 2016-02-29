eaa7cb7599b5ac0116b81f1e43fd0b3fc5a03b4e
Julian Eberius <julian@eberius-mail.de>
2016-02-29 23:07:00 +0100

    integrate iOS-Charts 2.2.2 initial win percentage graph

--- Track-o-Bot Companion/StatsViewController.swift
+++ Track-o-Bot Companion/StatsViewController.swift
@@ -7,17 +7,35 @@
 //
 
 import UIKit
+import Charts
 
 
 class StatsViewController: TrackOBotViewController {
-        
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    
+    @IBOutlet weak var chart: BarChartView!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.chart.noDataText = "Loading data ..."
         // check login
-        TrackOBot.instance.getDecks({
+        TrackOBot.instance.getByClassStats({
             (result) -> Void in
             switch result {
-            case .Success:
+            case .Success(let stats):
+                let data = stats.map { (d) -> BarChartDataEntry in
+                    let val = Double(d.wins) / Double(d.wins + d.losses) * 100.0
+                    return BarChartDataEntry(value: val, xIndex: HEROES.indexOf(d.hero)!)
+                }
+                let ds = BarChartDataSet(yVals: data, label: "Win %")
+                let d = BarChartData(xVals: HEROES, dataSet: ds)
+
+                self.chart.xAxis.labelRotationAngle = 90
+                self.chart.xAxis.labelPosition = ChartXAxis.XAxisLabelPosition.BottomInside
+                self.chart.xAxis.setLabelsToSkip(0)
+                self.chart.xAxis.spaceBetweenLabels = 0
+                self.chart.legend.position = ChartLegend.ChartLegendPosition.AboveChartLeft
+                self.chart.descriptionText = ""
+                self.chart.data = d
                 break
             case .Failure(let err):
                 switch err {
