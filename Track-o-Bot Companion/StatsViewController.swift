d63195c3b2278341f583aa85ec324e85e4e78866
Julian Eberius <julian@eberius-mail.de>
2016-03-26 23:21:35 +0100

    master / detail-style stats view, with overall win rate chart and "as-class / vs-class" detail chart on select

--- Track-o-Bot Companion/StatsViewController.swift
+++ Track-o-Bot Companion/StatsViewController.swift
@@ -12,8 +12,12 @@ import Charts
 
 class StatsViewController: TrackOBotViewController, ChartViewDelegate {
     
-    @IBOutlet weak var chart: BarChartView!
-    @IBOutlet weak var deckChart: BarChartView!
+    @IBOutlet weak var mainChartLabel: UILabel!
+    @IBOutlet weak var mainChart: BarChartView!
+    @IBOutlet weak var detailChartLabel: UILabel!
+    @IBOutlet weak var detailChart: BarChartView!
+    
+    var selectedIndexMainChart: Int? = nil
     
     let heroColors = [
         UIColor(red: 167/255.0, green: 57.0/255.0, blue: 45.0/255.0, alpha: 1.0),
@@ -27,17 +31,22 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
         UIColor(red: 250/255.0, green: 244/255.0, blue: 220.0/255.0, alpha: 1.0)
     ]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mainChart.noDataText = "Loading data ..."
+        self.detailChart.noDataText = "Select a class ..."
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        self.chart.noDataText = "Loading data ..."
-        self.deckChart.noDataText = "Loading data ..."
+        
 
         TrackOBot.instance.getByClassStats({
             (result) -> Void in
             switch result {
             case .Success(let stats):
                 let data = self.createHeroBarChartData(stats)
-                self.updateChart(self.chart, data: data, stats: stats)
+                self.updateChart(self.mainChart, data: data, stats: stats)
                 break
             case .Failure(let err):
                 switch err {
@@ -49,23 +58,10 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
             }
         })
         
-        TrackOBot.instance.getByDeckStats({
-            (result) -> Void in
-            switch result {
-            case .Success(let stats):
-                let data = self.createDeckBarChartData(stats)
-                self.updateChart(self.deckChart, data: data, stats: stats)
-                self.deckChart.xAxis.labelPosition = ChartXAxis.XAxisLabelPosition.BottomInside
-                break
-            case .Failure(let err):
-                switch err {
-                case .CredentialsMissing, .LoginFaild(_):
-                    self.performSegueWithIdentifier("to_login", sender: self)
-                default:
-                    print("what")
-                }
-            }
-        })
+        self.mainChart.highlightValue(nil)
+        self.selectedIndexMainChart = nil
+        self.detailChart.clear()
+        self.updateDescriptionLabel()
     }
     
     func createHeroBarChartData(stats: [ByClassStats]) -> BarChartData {
@@ -79,6 +75,9 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
         }
         let ds = BarChartDataSet(yVals: data, label: "Win %")
         ds.setColors(self.heroColors, alpha: 0.95)
+        ds.highlightAlpha = 0.0
+        ds.highlightLineWidth = 5.0
+        
         ds.drawValuesEnabled = false
         let d = BarChartData(xVals: HEROES, dataSet: ds)
         return d
@@ -101,66 +100,7 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
         return d
     }
     
-    class CustomChartMarker: ChartMarker
-    {
-        let stats: [Stats]!
-        let chart: ChartViewBase!
-        
-        var strVal: String = ""
-        var markerSize: CGSize = CGSize(width: 100, height: 15)
-        
-        init(chart:ChartViewBase, stats:[Stats])
-        {
-            self.chart = chart
-            self.stats = stats
-            super.init()
-        }
-        
-        
-        /// Draws the ChartMarker on the given position on the given context
-        override func draw(context context: CGContext, point: CGPoint)
-        {
-            let font = UIFont.systemFontOfSize(UIFont.systemFontSize())
-            let attrsDictionary = [NSFontAttributeName:font] as [String : AnyObject]
-            let str = NSAttributedString(string: strVal, attributes: attrsDictionary)
-            let rectSize = str.boundingRectWithSize(CGSize(width: 300, height: 20), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
-            markerSize = rectSize.size
-            
-            var offset = CGPoint(x: 0, y: 0)
-            if let width = chart.viewPortHandler?.chartWidth {
-                if (point.x + rectSize.width > width) {
-                    offset = CGPoint(x: -1.0 * rectSize.width, y: 0)
-                }
-            }
-
-            let rect = CGRect(x: point.x + offset.x, y: point.y + offset.y, width: rectSize.width + 6, height: rectSize.height + 4)
-            
-            UIGraphicsPushContext(context)
-            CGContextSetLineWidth(context, 1.0)
-            CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
-            CGContextSetFillColor(context, CGColorGetComponents(UIColor.whiteColor().CGColor))
-            CGContextFillRect(context, rect)
-            CGContextStrokeRect(context, rect)
-            UIGraphicsPopContext()
-            NSString(string: strVal).drawInRect(rect.offsetBy(dx: 3, dy: 2), withAttributes: attrsDictionary)
-        }
-        
-        /// This method enables a custom ChartMarker to update it's content everytime the MarkerView is redrawn according to the data entry it points to.
-        ///
-        /// - parameter highlight: the highlight object contains information about the highlighted value such as it's dataset-index, the selected range or stack-index (only stacked bar entries).
-        override func refreshContent(entry entry: ChartDataEntry, highlight: ChartHighlight)
-        {
-            let s = stats[entry.xIndex]
-            let percentStr = String(format: "%.1f", entry.value)
-            strVal = "\(percentStr)% (W: \(s.wins) / L: \(s.losses))"
-        }
-        
-        override var size: CGSize {
-            get { return markerSize }
-        }
-    }
-    
-    func updateChart(chart: BarLineChartViewBase, data: ChartData, stats: [Stats]) {
+    func updateChart(chart: BarChartView, data: ChartData, stats: [Stats]) {
         if (self.view.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.Compact) {
             chart.xAxis.labelRotationAngle = 90
         } else {
@@ -171,7 +111,6 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
         chart.xAxis.setLabelsToSkip(0)
         chart.xAxis.spaceBetweenLabels = 0
 
-        //chart.xAxis.valueFormatter = CustomXAxisValueFormatter(stats: stats)
         chart.rightAxis.enabled = false
         chart.leftAxis.customAxisMax = 100
         chart.leftAxis.customAxisMin = 0
@@ -183,18 +122,55 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
         chart.dragEnabled = false
         chart.descriptionText = ""
         chart.delegate = self
+        chart.drawHighlightArrowEnabled = true
+    
         chart.marker = CustomChartMarker(chart: chart, stats: stats)
 
         chart.data = data
-        chart.setNeedsDisplay()
+        chart.animate(yAxisDuration: 0.75)
     }
     
     func chartValueSelected(chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: ChartHighlight) {
-        
+        if (chartView == self.detailChart) {
+            return
+        }
+        if (self.selectedIndexMainChart != entry.xIndex) {
+            TrackOBot.instance.getVsClassStats(HEROES[entry.xIndex], onComplete: {
+                (result) -> Void in
+                switch result {
+                case .Success(let stats):
+                    let data = self.createHeroBarChartData(stats)
+                    self.updateChart(self.detailChart, data: data, stats: stats)
+                    self.selectedIndexMainChart = entry.xIndex
+                    self.updateDescriptionLabel()
+                    break
+                case .Failure(let err):
+                    switch err {
+                    case .CredentialsMissing, .LoginFaild(_):
+                        self.performSegueWithIdentifier("to_login", sender: self)
+                    default:
+                        print("what")
+                    }
+                }
+            })
+        }
     }
     
     func chartValueNothingSelected(chartView: ChartViewBase) {
         
+    }
+    
+    func updateDescriptionLabel() {
+        guard let idx = self.selectedIndexMainChart else {
+            self.detailChartLabel.text = "Win rates as ..."
+            return
+        }
+        let attrString = NSMutableAttributedString(string: "Win rates as ")
+        let heroString = NSAttributedString(string: "\(HEROES[idx])",
+                                            attributes: [NSForegroundColorAttributeName: heroColors[idx],
+                                                NSFontAttributeName: UIFont.boldSystemFontOfSize(self.detailChartLabel.font.pointSize)])
+        attrString.appendAttributedString(heroString)
+        self.detailChartLabel.attributedText = attrString
     }
     
     @IBAction func unwindFromLogin(unwindSegue: UIStoryboardSegue) {
@@ -202,16 +178,66 @@ class StatsViewController: TrackOBotViewController, ChartViewDelegate {
     }
 }
 
-class CustomXAxisValueFormatter : ChartXAxisValueFormatter {
-    
+class CustomChartMarker: ChartMarker
+{
     let stats: [Stats]!
+    let chart: ChartViewBase!
     
-    init(stats: [Stats]) {
+    var strVal: String = ""
+    var markerSize: CGSize = CGSize(width: 100, height: 15)
+    
+    init(chart:ChartViewBase, stats:[Stats])
+    {
+        self.chart = chart
         self.stats = stats
+        super.init()
     }
     
-    @objc func stringForXValue(index: Int, original: String, viewPortHandler: ChartViewPortHandler) -> String {
-        let s = stats[index]
-        return original + "\(s.wins) wins, \(s.losses) losses"
+    
+    /// Draws the ChartMarker on the given position on the given context
+    override func draw(context context: CGContext, point: CGPoint)
+    {
+        let font = UIFont.systemFontOfSize(UIFont.systemFontSize())
+        let attrsDictionary = [NSFontAttributeName:font] as [String : AnyObject]
+        let str = NSAttributedString(string: strVal, attributes: attrsDictionary)
+        let rectSize = str.boundingRectWithSize(CGSize(width: 300, height: 20), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+        markerSize = rectSize.size
+        
+        var offset = CGPoint(x: 22, y: -19.0)
+        if let width = chart.viewPortHandler?.chartWidth {
+            if (point.x + rectSize.width + offset.x > width) {
+                offset = CGPoint(x: -1.0 * rectSize.width, y: 0)
+            }
+            
+        }
+        
+        if (point.y + offset.y < 0) {
+            offset = CGPoint(x: 0, y: 0 )
+        }
+        
+        let rect = CGRect(x: point.x + offset.x, y: point.y + offset.y, width: rectSize.width + 6, height: rectSize.height + 4)
+        
+        UIGraphicsPushContext(context)
+        CGContextSetLineWidth(context, 1.0)
+        CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+        CGContextSetFillColor(context, CGColorGetComponents(UIColor.whiteColor().CGColor))
+        CGContextFillRect(context, rect)
+        CGContextStrokeRect(context, rect)
+        UIGraphicsPopContext()
+        NSString(string: strVal).drawInRect(rect.offsetBy(dx: 3, dy: 2), withAttributes: attrsDictionary)
+    }
+    
+    /// This method enables a custom ChartMarker to update it's content everytime the MarkerView is redrawn according to the data entry it points to.
+    ///
+    /// - parameter highlight: the highlight object contains information about the highlighted value such as it's dataset-index, the selected range or stack-index (only stacked bar entries).
+    override func refreshContent(entry entry: ChartDataEntry, highlight: ChartHighlight)
+    {
+        let s = stats[entry.xIndex]
+        let percentStr = String(format: "%.1f", entry.value)
+        strVal = "\(percentStr)% (W: \(s.wins) / L: \(s.losses))"
+    }
+    
+    override var size: CGSize {
+        get { return markerSize }
     }
 }
